631188489bd333802e862e57413aa6cb45cb6e58
Aashish Shrestha <aashishshrestha7035@gmail.com>
2025-05-16 20:26:41 -0400

    Refactor insertion sort implementation with improved function structure and argument parsing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 
 def main(items):
+    """
+    Sorts the array in-place in monotonically decreasing order using Insertion Sort.
+    """
     for i in range(1, len(items)):
         key = items[i]
         j = i - 1
@@ -9,17 +12,22 @@ def main(items):
             j -= 1
         items[j + 1] = key
 
-
-if __name__ == "__main__":
+def parse_input_args():
+    """
+    Parses command line arguments into a list of integers.
+    Returns the list or exits if invalid input is provided.
+    """
     if len(sys.argv) > 1:
         try:
-            items = list(map(int, sys.argv[1:]))
+            return list(map(int, sys.argv[1:]))
         except ValueError:
-            print("Please enter a list of integers separated by spaces.")
+            print("Error: Please enter a list of integers separated by spaces.")
             sys.exit(1)
     else:
-        items = [5, 2, 9, 1, 5, 6]
+        return [5, 2, 9, 1, 5, 6]
 
+if __name__ == "__main__":
+    items = parse_input_args()
     print("Original array:", items)
     main(items)
     print("Sorted array (decreasing):", items)
